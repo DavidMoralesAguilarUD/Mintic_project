a22611ccd84fd6d197c0d77840aa64cfd3aa76e2
DavidMoralesAguilarUD <davidtrokten@gmail.com>
2024-10-09 18:43:52 -0500

    Actualizar VPC

--- main.tf
+++ main.tf
@@ -18,11 +18,29 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
+
+resource "aws_vpc" "example_vpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "example-vpc"
+  }
+}
+
+resource "aws_subnet" "example_subnet" {
+  vpc_id     = aws_vpc.example_vpc.id
+  cidr_block = "10.0.1.0/24"
+  
+  tags = {
+    Name = "example-subnet"
+  }
+}
+
 resource "aws_instance" "example" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
   key_name      = var.key_name
-
+  subnet_id     = aws_subnet.example_subnet.id  # Asocia la instancia a la Subnet
   tags = {
     Name = var.name
   }
--- variables.tf
+++ variables.tf
@@ -13,10 +13,6 @@ variable "key_name" {
   default = "2023"
 }
 
-variable "s3_bucket_name" {
-  description = "Name of the S3 bucket for remote state"
-  default = "tfdemo16112023"
-}
 
 variable "name" {
   description = "Name of EC2 Intance"
